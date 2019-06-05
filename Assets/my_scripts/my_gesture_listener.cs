70e45c41e0fd276bebb9629c200eda9e98a5799d
Krisskr <546628725@qq.com>
2019-06-05 20:26:17 +0800

    初次提交

---
+++ Assets/my_scripts/my_gesture_listener.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class my_gesture_listener : MonoBehaviour, VisualGestureListenerInterface
+{
+	//动作姿态完成
+	public bool GestureCompleted(long userId, int userIndex, string gesture, float confidence)
+	{
+		//姿势检测后触发的事件在这里写
+		if (gesture == "touch_jj") 
+		{
+			print("摸鸡鸡...");
+		}
+        if (gesture == "hands_over_head")
+        {
+            print("手举过了头...");
+        }
+        return true;
+		//throw new System.NotImplementedException();
+	}
+	//检测动作姿态过程
+	public void GestureInProgress(long userId, int userIndex, string gesture, float progress)
+	{
+
+	}
+	void Start () {
+
+	}
+	void Update () {
+
+	}
+}
